cb6e154cf8e5ce10a20e1df115ac46df943c0ea6
intbusinessanto <intbusinessanto@hotmail.com>
2022-02-10 13:21:21 -0800

    X commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+** main - prints the size of various types
+** Return: 0 if exited property, non zero otherwise
+**/
+int main(void)
+{
+printf("Size of a char:%d bytes(s)\n", sizeof(char));
+printf("Size of an int:%d bytes(s)\n", sizeof(int));
+printf("Size of a long int:%d bytes(s)\n", sizeof(long int));
+printf("Size of a long long int:%d bytes(s)\n", sizeof(long long int));
+printf("Size of a float:%d bytes(s)\n", sizeof(float));
+}

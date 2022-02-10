7ae040f6b606d889dcc34f3fb3b85c6804590786
intbusinessanto <intbusinessanto@hotmail.com>
2022-02-10 11:32:37 -0800

    X commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
+ *Comment
  *
- * */
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

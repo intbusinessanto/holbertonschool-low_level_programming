49062ed8300a9c06a5d87447cc3f6400995111c5
intbusinessanto <intbusinessanto@hotmail.com>
2022-02-11 13:44:01 -0800

    X commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+** main-main block
+** Return: 0
+**/
+
+int main(void)
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+if (i == 'e' || i == 'q')
+{
+putchar(' ');
+}
+
+putchar('\n');
+
+
+return (0);
+}

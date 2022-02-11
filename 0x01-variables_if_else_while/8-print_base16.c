ed38e48739bd7eafeca637162c9f1ef0767ae4d3
intbusinessanto <intbusinessanto@hotmail.com>
2022-02-11 15:12:47 -0800

    X commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+** main-main block
+** Return: 0
+**/
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i <= 10; i++)
+putchar(i);
+for (i = 'a'; i <= 'g'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}

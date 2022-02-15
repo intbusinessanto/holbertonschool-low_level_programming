c85b2497cb9531e5b9217c5fc19a84c60b5e76c3
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-15 15:38:53 -0500

    x

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,8 +4,6 @@
 ** add - writes the character c to stdout
 ** @n1: The character to print
 ** @n2: The character to print
-** @r: The character to print
-**
 ** Return: On success 1.
 ** On error, -1 is returned, and errno is set appropriately.
 **/

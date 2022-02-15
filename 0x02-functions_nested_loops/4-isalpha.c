eb44faa4bc306ead2f9e070e02b5b02b38d263ce
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-15 13:40:27 -0500

    x

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-** _putchar - writes the character c to stdout
+** _isalpha - writes the character c to stdout
 ** @c: The character to print
 **
 ** Return: On success 1.
@@ -10,11 +10,11 @@
 
 int _isalpha(int c)
 {
-    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-    {
-        return (1);
-    }
-    else{
-        return (0);
-    }
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+{
+return (1);
+}
+else{
+return (0);
+}
 }

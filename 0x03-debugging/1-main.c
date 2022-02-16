172f13388e546f3f931bd25417ee2fb741b0d64d
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-16 13:42:47 -0500

    x

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,18 +8,14 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 /**
 ** while (i < 10)
 ** {
 ** putchar(i);
 ** }
-** \\o/\n"
 **/
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }

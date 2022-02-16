361d4c1274317927aea36b0acfaf009f42e65ff3
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-16 14:58:37 -0500

    x

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,12 +11,8 @@
 ** Return: n and evaluates against logical operators
 **/
 
-int main(void)
+int positive_or_negative(int n)
 {
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {

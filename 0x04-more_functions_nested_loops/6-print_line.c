42f5f528e0f33e60c9d3f9a56d5f331267afe5d6
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-17 18:42:36 -0500

    x

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-** more_numbers - writes the character c to stdout
+** print_line - writes the character c to stdout
 **
 ** Return: On success 1.
 ** On error, -1 is returned, and errno is set appropriately.

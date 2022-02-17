455cdec137f1547fabd7ac4b3ecc3eebb3be467f
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-17 11:47:49 -0500

    x

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@
 
 void print_most_numbers(void)
 {
-char c = '/';
+int c;
 
-while ((c++) < '9')
-{
-if (c != '2' || c != '2')
-_putchar(c);
+for (c = 0; c <= 9; c++){
+    if (c != 2 && c != 4)
+    _putchar(c + '0');
 }
+
 _putchar('\n');
 }

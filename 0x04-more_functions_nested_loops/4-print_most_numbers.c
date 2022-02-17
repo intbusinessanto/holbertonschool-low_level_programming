7b3a617d26c00de1df28424f16df5363300f96f6
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-17 11:14:39 -0500

    x

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,23 +2,19 @@
 #include "main.h"
 
 /**
-** _isdigit - writes the character c to stdout
-** @a: The character to print
-** @b: The character to print
-** @r: The character to print
-**
+** print_most_numbers - writes the character c to stdout
 ** Return: On success 1.
 ** On error, -1 is returned, and errno is set appropriately.
 **/
 
 void print_most_numbers(void)
 {
-    char c = '/';
+char c = '/';
 
-    while((c++) < '9')
-    {
-        if (c != '2' || c != '2')
-        _putchar(c);
-    }
-    _putchar('\n');
+while((c++) < '9')
+{
+if (c != '2' || c != '2')
+_putchar(c);
+}
+_putchar('\n');
 }

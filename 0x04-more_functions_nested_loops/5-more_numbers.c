bc869370fca44a3940fbc7f0167813061d5a35a2
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-17 17:58:35 -0500

    x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,19 @@
 
 void more_numbers(void)
 {
-    int c;
-    int s;
+int c;
+int s;
 
-    for (s = 0; s <= 10; s++){
-        for (c = 0; c <= 14; c++){
-            if (c >= 10){
-                _putchar((c / 10) + '0');
-            }
-            _putchar((c % 10) + '0');
-        }
-    _putchar('\n');
-    }   
+for (s = 0; s <= 10; s++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c >= 10)
+{
+_putchar((c / 10) + '0');
+}
+_putchar((c % 10) + '0');
+}
+_putchar('\n');
+}
 }

3f76413c88e09c261ad2466515887f8c8afedf60
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-17 11:09:48 -0500

    x

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 
 /**
 ** print_numbers - writes the character c to stdout
-**
 ** Return: On success 1.
 ** On error, -1 is returned, and errno is set appropriately.
 **/
@@ -11,7 +10,6 @@
 void print_numbers(void)
 {
 char c = '/';
-
 while ((c++) < '9')
 _putchar(c);
 }

d0e7687aacab4a59e0b8775c737508e6e121ccc5
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-21 17:38:44 -0500

    x

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer to string to measure
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+   	int len = 0;
+
+    for (i = 0; *(s + i) != '\0'; i++)
+	{
+		len = len + 1;
+	}
+	return (len);
+}

abad5519d2cf37d71694a7bc26c03cc9ce42da55
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-21 17:40:22 -0500

    x

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,12 +10,10 @@
 
 int _strlen(char *s)
 {
-	int i;
-   	int len = 0;
+int i;
+int len = 0;
 
-    for (i = 0; *(s + i) != '\0'; i++)
-	{
-		len = len + 1;
-	}
-	return (len);
+for (i = 0; *(s + i) != '\0'; i++)
+len = len + 1;
+return (len);
 }

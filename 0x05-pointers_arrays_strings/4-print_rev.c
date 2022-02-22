e792c18b136edd999e6a1e3c4089cdb2c4b9112f
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-21 20:22:28 -0500

    x

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string, followed by a new line, to stdout.
+ * @s: Pointer to string to print
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+
+int i;
+int t = 0;
+
+while (*s != '\0')
+{
+s = s + 1;
+t++;
+}
+for (i = 1; i <= t; i++)
+{
+_putchar(*(s-i));
+}
+_putchar('\n');
+
+}

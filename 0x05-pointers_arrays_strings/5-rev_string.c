fbfa7e5a60570ecab2f87003af5674184fe9bc6c
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-21 21:07:45 -0500

    x

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints a string, followed by a new line, to stdout.
+ * @s: Pointer to string to print
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+char *i = s;
+char temp;
+
+while (*s != '\0'){
+s = s + 1;
+}
+s = s - 1;
+
+while(i <= s){
+temp = *i;
+*i = *s;
+*s = temp;
+
+i = i + 1;
+s = s - 1;
+}
+}

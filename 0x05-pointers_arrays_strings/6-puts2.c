b1db900049200c5840e0c80798a804b6155a362d
Santiago Rodríguez <intbusinessanto@hotmail.com>
2022-02-21 21:20:27 -0500

    x

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints a string, followed by a new line, to stdout.
+ * @s: Pointer to string to print
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+int lenght = 0;
+
+while (*str != '\0')
+{
+if ((lenght % 2) == 0)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
